e3797619641f95b561af305d7d9087de5bb42919
Tom Witkowski <dev.gummibeer@gmail.com>
2026-08-28 17:57:48 +0200

    update Prettier configuration with Blade and TailwindCSS plugins, remove unnecessary formatting step from GitHub workflow

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -5,7 +5,24 @@ export default {
     singleQuote: true,
     singleAttributePerLine: true,
     objectWrap: 'preserve',
+    plugins: [
+        'prettier-plugin-blade',
+        'prettier-plugin-tailwindcss',
+    ],
+    tailwindStylesheet: './resources/css/app.css',
     overrides: [
+        {
+            files: '*.blade.php',
+            options: {
+                parser: 'blade',
+                bladeKeepHeadAndBodyAtRoot: true,
+                bladeVoidElementSlash: 'always',
+                bladeEchoSpacing: 'space',
+                bladeDirectiveArgSpacing: 'space',
+                bladeDirectiveBlockStyle: 'preserve',
+                bladeBlankLinesAroundDirectives: 'preserve',
+            },
+        },
         {
             files: ['*.yml', '*.yaml'],
             options: {
