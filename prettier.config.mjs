80157cdfc49c86d8e7df57b18175409dca9b9783
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-28 17:17:54 +0200

    Decouple frontend formatting from Blade plugins

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -5,24 +5,7 @@ export default {
     singleQuote: true,
     singleAttributePerLine: true,
     objectWrap: 'preserve',
-    plugins: [
-        'prettier-plugin-blade',
-        'prettier-plugin-tailwindcss',
-    ],
-    tailwindStylesheet: './resources/css/app.css',
     overrides: [
-        {
-            files: '*.blade.php',
-            options: {
-                parser: 'blade',
-                bladeKeepHeadAndBodyAtRoot: true,
-                bladeVoidElementSlash: 'always',
-                bladeEchoSpacing: 'space',
-                bladeDirectiveArgSpacing: 'space',
-                bladeDirectiveBlockStyle: 'preserve',
-                bladeBlankLinesAroundDirectives: 'preserve',
-            },
-        },
         {
             files: ['*.yml', '*.yaml'],
             options: {
