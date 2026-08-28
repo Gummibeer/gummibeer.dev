d3c1052326d826385de74967d78efb5754d65a6d
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-28 09:44:52 +0200

    Restore Laravel web and API middleware groups

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,8 +2,13 @@
 
 namespace App\Http;
 
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class Kernel extends HttpKernel
 {
@@ -11,9 +16,18 @@ class Kernel extends HttpKernel
 
     protected $middlewareGroups = [
         'web' => [
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
+            ShareErrorsFromSession::class,
+            ValidateCsrfToken::class,
+            SubstituteBindings::class,
+        ],
+
+        'api' => [
             SubstituteBindings::class,
         ],
     ];
 
-    protected $routeMiddleware = [];
+    protected $middlewareAliases = [];
 }
