955b00edce25eb5ba05f44acf5f934881de8a200
Tom Witkowski <dev.gummibeer@gmail.com>
2026-08-28 18:04:39 +0200

    apply consistent quote style in CSS, JS, and config files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,57 +1,57 @@
-import Alpine from "alpinejs";
-import ClipboardJS from "clipboard";
-import Prism from "prismjs";
-import "prismjs/components/prism-markup-templating";
-import "prismjs/components/prism-php";
-import "prismjs/components/prism-ini";
-import "prismjs/components/prism-scss";
-import "prismjs/plugins/line-numbers/prism-line-numbers";
-import twemoji from "@twemoji/api";
+import Alpine from 'alpinejs';
+import ClipboardJS from 'clipboard';
+import Prism from 'prismjs';
+import 'prismjs/components/prism-markup-templating';
+import 'prismjs/components/prism-php';
+import 'prismjs/components/prism-ini';
+import 'prismjs/components/prism-scss';
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
+import twemoji from '@twemoji/api';
 
-Alpine.data("slider", (delay = 3) => ({
-  delay: delay * 1000,
-  images: [],
-  index: 0,
-  init() {
-    this.images = Array.from(this.$el.getElementsByTagName("img"));
-    this.render();
-    window.setInterval(() => this.next(), this.delay);
-  },
-  next() {
-    this.index = this.index < this.images.length - 1 ? this.index + 1 : 0;
-    this.render();
-  },
-  render() {
-    this.images.forEach((image, index) => {
-      image.classList.toggle("hidden", index !== this.index);
-    });
-  },
+Alpine.data('slider', (delay = 3) => ({
+    delay: delay * 1000,
+    images: [],
+    index: 0,
+    init() {
+        this.images = Array.from(this.$el.getElementsByTagName('img'));
+        this.render();
+        window.setInterval(() => this.next(), this.delay);
+    },
+    next() {
+        this.index = this.index < this.images.length - 1 ? this.index + 1 : 0;
+        this.render();
+    },
+    render() {
+        this.images.forEach((image, index) => {
+            image.classList.toggle('hidden', index !== this.index);
+        });
+    },
 }));
 
 window.Alpine = Alpine;
 Alpine.start();
 
 Prism.highlightAll();
-new ClipboardJS("button[data-clipboard-text]");
+new ClipboardJS('button[data-clipboard-text]');
 
 window.search = {
-  query: "",
-  results: [],
-  search() {
-    const url = new URL("https://search.gummibeer.dev");
-    url.searchParams.set("q", this.query);
-    url.searchParams.set("t", Date.now());
+    query: '',
+    results: [],
+    search() {
+        const url = new URL('https://search.gummibeer.dev');
+        url.searchParams.set('q', this.query);
+        url.searchParams.set('t', Date.now());
 
-    fetch(url)
-      .then((response) => response.json())
-      .then((results) => {
-        this.results = results
-.toSorted((a, b) => a.score - b.score)
-.slice(0, 3)
-.map(({ item }) => item);
-      });
-  },
+        fetch(url)
+            .then((response) => response.json())
+            .then((results) => {
+                this.results = results
+                    .toSorted((a, b) => a.score - b.score)
+                    .slice(0, 3)
+                    .map(({ item }) => item);
+            });
+    },
 };
 
-window.twemoji = (content) => twemoji.parse(content, { folder: "svg", ext: ".svg" });
+window.twemoji = (content) => twemoji.parse(content, { folder: 'svg', ext: '.svg' });
 window.twemoji(document.body);
