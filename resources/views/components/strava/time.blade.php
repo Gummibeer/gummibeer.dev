62d443522e3cfda8e41d50d720f9661664341374
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-28 12:47:24 +0200

    Use file-backed Strava stats

--- resources/views/components/strava/time.blade.php
+++ resources/views/components/strava/time.blade.php
@@ -1,6 +1,6 @@
 <x-strava.card
     icon="fa-stopwatch"
     label="Time"
-    :value="sheets('strava')->get(config('services.strava.athlete_id'))->moving_time / 60 / 60"
+    :value="app(App\Services\StravaStats::class)->movingTime() / 60 / 60"
     unit="h"
 />
