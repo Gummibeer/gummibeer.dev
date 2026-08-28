575ad120c8270477089c1a6b57e9c217a6b65f10
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-28 16:33:16 +0200

    Use Statamic Markdown in figure captions

--- resources/views/components/figure.blade.php
+++ resources/views/components/figure.blade.php
@@ -1,15 +1,12 @@
 <?php /** @var Illuminate\View\ComponentAttributeBag $attributes */ ?>
 <?php /** @var Illuminate\Support\HtmlString $slot */ ?>
 <?php /** @var Illuminate\Support\HtmlString $caption */ ?>
-<?php /** @var League\CommonMark\ConverterInterface $markdown */ ?>
-
-@inject('markdown', 'markdown')
 
 <figure @if(!empty((string) $caption)) role="group" @endif {{ $attributes->merge(['class' => 'overflow-hidden']) }}>
     {{ $slot }}
     @if(!empty((string) $caption))
         <figcaption class="mt-1 text-sm text-center text-snow-20 dark:text-snow-10">
-            {!! $markdown->convert((string) $caption) !!}
+            {!! \Statamic\Facades\Markdown::parse((string) $caption) !!}
         </figcaption>
     @endif
 </figure>
