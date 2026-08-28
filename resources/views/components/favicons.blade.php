482046dd4646d662b2d4e0bddc186e6ed229d7cf
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-28 18:42:09 +0200

    Serve favicons through signed Statamic URLs

--- resources/views/components/favicons.blade.php
+++ resources/views/components/favicons.blade.php
@@ -1,35 +1,34 @@
 {{-- https://favicon.io/emoji-favicons/panda --}}
+@php
+    $glide = app(\Statamic\Contracts\Imaging\UrlBuilder::class);
+    $favicon = static fn (string $path): string => $glide->build('images::favicons/'.$path, []);
+@endphp
 <link
     rel="apple-touch-icon"
     sizes="180x180"
-    href="{{ asset('images/favicons/apple-touch-icon.png') }}"
+    href="{{ $favicon('apple-touch-icon.png') }}"
 />
 <link
     rel="icon"
     type="image/png"
     sizes="512x512"
-    href="{{ asset('images/favicons/favicon-512x512.png') }}"
+    href="{{ $favicon('favicon-512x512.png') }}"
 />
 <link
     rel="icon"
     type="image/png"
     sizes="192x192"
-    href="{{ asset('images/favicons/favicon-192x192.png') }}"
+    href="{{ $favicon('favicon-192x192.png') }}"
 />
 <link
     rel="icon"
     type="image/png"
     sizes="32x32"
-    href="{{ asset('images/favicons/favicon-32x32.png') }}"
+    href="{{ $favicon('favicon-32x32.png') }}"
 />
 <link
     rel="icon"
     type="image/png"
     sizes="16x16"
-    href="{{ asset('images/favicons/favicon-16x16.png') }}"
-/>
-<link
-    rel="shortcut icon"
-    type="image/x-icon"
-    href="{{ asset('images/favicons/favicon.ico') }}"
+    href="{{ $favicon('favicon-16x16.png') }}"
 />
