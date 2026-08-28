0380ffa188776d9ac40e133859b00cc45f0446a4
Tom Witkowski <dev.gummibeer@gmail.com>
2026-08-28 18:04:26 +0200

    update Prettier configuration: refine Blade plugin options and adjust overrides

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,23 +1,30 @@
-import blade from "prettier-plugin-blade";
-import tailwindcss from "prettier-plugin-tailwindcss";
-
 export default {
-  printWidth: 999999,
-  plugins: [blade, tailwindcss],
-  proseWrap: "preserve",
-  tabWidth: 4,
-  singleQuote: true,
-  singleAttributePerLine: true,
-  objectWrap: "preserve",
-  tailwindStylesheet: "./resources/css/app.css",
-  overrides: [
-    {
-      files: ["*.blade.php"],
-      options: { parser: "blade" },
-    },
-    {
-      files: ["*.yml", "*.yaml"],
-      options: { tabWidth: 2 },
-    },
-  ],
+    printWidth: 999999,
+    proseWrap: 'preserve',
+    tabWidth: 4,
+    singleQuote: true,
+    singleAttributePerLine: true,
+    objectWrap: 'preserve',
+    plugins: ['prettier-plugin-blade', 'prettier-plugin-tailwindcss'],
+    tailwindStylesheet: './resources/css/app.css',
+    overrides: [
+        {
+            files: '*.blade.php',
+            options: {
+                parser: 'blade',
+                bladeKeepHeadAndBodyAtRoot: true,
+                bladeVoidElementSlash: 'always',
+                bladeEchoSpacing: 'space',
+                bladeDirectiveArgSpacing: 'space',
+                bladeDirectiveBlockStyle: 'preserve',
+                bladeBlankLinesAroundDirectives: 'preserve',
+            },
+        },
+        {
+            files: ['*.yml', '*.yaml'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
 };
