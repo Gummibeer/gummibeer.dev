a9589c644d84a5a0bd40890b68cea5f77d721665
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 00:23:34 +0200

    Use CMS identity in website metadata

--- app/View/Components/Og/Website.php
+++ app/View/Components/Og/Website.php
@@ -3,6 +3,7 @@
 namespace App\View\Components\Og;
 
 use App\Services\MetaBag;
+use App\Services\SiteIdentity;
 use Astrotomic\OpenGraph\OpenGraph;
 use Astrotomic\OpenGraph\Twitter;
 use Illuminate\View\Component;
@@ -11,9 +12,12 @@ class Website extends Component
 {
     protected MetaBag $meta;
 
-    public function __construct(MetaBag $meta)
+    protected SiteIdentity $identity;
+
+    public function __construct(MetaBag $meta, SiteIdentity $identity)
     {
         $this->meta = $meta;
+        $this->identity = $identity;
     }
 
     public function render(): string
@@ -27,8 +31,8 @@ class Website extends Component
             Twitter::summaryLargeImage($this->meta->title)
                 ->when($this->meta->description)->description($this->meta->description)
                 ->when($this->meta->image)->image($this->meta->image)
-                ->site(config('app.name'))
-                ->creator('@devgummibeer'),
+                ->site($this->identity->siteName())
+                ->creator($this->identity->twitterHandle()),
         ]);
     }
 }
