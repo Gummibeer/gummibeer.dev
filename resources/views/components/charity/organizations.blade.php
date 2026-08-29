9efa6d436a7cea3fe468b0f46b383e1d7bbc48ee
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 11:16:55 +0200

    Move charity query into component

---
+++ resources/views/components/charity/organizations.blade.php
@@ -0,0 +1,23 @@
+@php
+    $charities = Statamic\Facades\Entry::query()
+        ->where('collection', 'charities')
+        ->whereStatus('published')
+        ->get()
+        ->sortBy(fn ($entry) => (string) $entry->value('title'))
+        ->values();
+@endphp
+
+<x-section class="bg-dotted">
+    <x-grid class="xl:grid-cols-4">
+        @foreach ($charities as $charity)
+            <div class="overflow-hidden rounded-4 bg-white shadow dark:bg-night-20">
+                <a href="{{ $charity->value('url') }}" target="_blank" rel="noreferrer noopener" class="block pb-1">
+                    <x-figure>
+                        <x-img :src="$charity->image" width="768" ratio="16:9" :alt="$charity->value('title')" />
+                        <x-slot name="caption">{{ $charity->value('title') }}</x-slot>
+                    </x-figure>
+                </a>
+            </div>
+        @endforeach
+    </x-grid>
+</x-section>
