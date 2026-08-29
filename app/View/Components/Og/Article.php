88068c72f1a518ca887cfd9f68c0a88e7a8d269f
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 15:42:23 +0200

    Use post data directly for article metadata

--- app/View/Components/Og/Article.php
+++ app/View/Components/Og/Article.php
@@ -2,7 +2,6 @@
 
 namespace App\View\Components\Og;
 
-use App\Services\MetaBag;
 use App\Services\SiteIdentity;
 use Astrotomic\OpenGraph\OpenGraph;
 use Astrotomic\OpenGraph\Twitter;
@@ -13,40 +12,33 @@ use Statamic\Facades\Entry;
 
 class Article extends Component
 {
-    protected MetaBag $meta;
-
     protected EntryContract $post;
 
     protected SiteIdentity $identity;
 
-    public function __construct(MetaBag $meta, EntryContract $post, SiteIdentity $identity)
+    public function __construct(EntryContract $post, SiteIdentity $identity)
     {
-        $this->meta = $meta;
         $this->post = $post;
         $this->identity = $identity;
     }
 
     public function render(): string
     {
+        $title = $this->post->value('title').' | '.$this->identity->siteName();
+        $description = (string) $this->post->value('description');
         $author = Entry::find((string) $this->post->value('author'));
-        $authorUrl = $author instanceof EntryContract
-            ? route('blog.author.index', ['author' => $author->slug()])
-            : url('/');
         $twitter = $author instanceof EntryContract ? (string) $author->value('twitter') : '';
 
         return implode(PHP_EOL, [
-            OpenGraph::article($this->meta->title)
-                ->url(url()->current())
-                ->when($this->meta->description)->description($this->meta->description)
-                ->author($authorUrl)
+            OpenGraph::article($title)
+                ->url((string) $this->post->absoluteUrl())
+                ->when($description)->description($description)
                 ->publishedAt($this->post->date())
                 ->modifiedAt(Carbon::createFromTimestampUTC(filemtime($this->post->path())))
-                ->when($this->meta->image)->image($this->meta->image)
                 ->locale(str_replace('-', '_', app()->getLocale())),
-            Twitter::summaryLargeImage($this->meta->title)
-                ->when($this->meta->description)->description($this->meta->description)
-                ->when($this->meta->image)->image($this->meta->image)
-                ->site($this->identity->siteName())
+            Twitter::summaryLargeImage($title)
+                ->when($description)->description($description)
+                ->site($this->identity->twitterHandle())
                 ->when($twitter)->creator($twitter),
         ]);
     }
