66b38ca0324f749b4ebd0e7c885db8dccc4b6e87
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 13:07:29 +0200

    Remove obsolete collection pagination mixin

--- app/Providers/MacroServiceProvider.php
+++ app/Providers/MacroServiceProvider.php
@@ -2,25 +2,14 @@
 
 namespace App\Providers;
 
-use App\Macros\CollectionMixin;
 use App\Macros\StrMixin;
-use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
-use Illuminate\Support\Traits\Macroable;
 
 class MacroServiceProvider extends ServiceProvider
 {
-    private const MIXINS = [
-        Collection::class => CollectionMixin::class,
-        Str::class => StrMixin::class,
-    ];
-
     public function boot(): void
     {
-        /** @var Macroable $class */
-        foreach (self::MIXINS as $class => $mixin) {
-            forward_static_call([$class, 'mixin'], $this->app->make($mixin));
-        }
+        Str::mixin($this->app->make(StrMixin::class));
     }
 }
