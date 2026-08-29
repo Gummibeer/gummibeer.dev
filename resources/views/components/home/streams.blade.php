9351346d9bf5d687182505dde2a899b779bc919f
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 11:20:04 +0200

    Keep home streams Blade presentation-only

--- resources/views/components/home/streams.blade.php
+++ resources/views/components/home/streams.blade.php
@@ -1,12 +1,3 @@
-@php
-    $streams = Statamic\Facades\Entry::query()
-        ->where('collection', 'streams')
-        ->whereStatus('published')
-        ->get()
-        ->sortByDesc(fn (Statamic\Entries\Entry $entry) => $entry->date())
-        ->values();
-@endphp
-
 <x-section>
     <h2 class="mb-8 text-4xl leading-none font-bold text-night-0 dark:text-white">Latest Streams</h2>
     <x-grid>
