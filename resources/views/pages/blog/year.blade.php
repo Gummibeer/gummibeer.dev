b63198f9fbd989e04296450320b918db23021245
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 13:06:46 +0200

    Use Statamic collection pagination for year archives

--- resources/views/pages/blog/year.blade.php
+++ resources/views/pages/blog/year.blade.php
@@ -1,21 +1,31 @@
-<?php /** @var App\Services\Paginator $posts */ ?>
 <?php /** @var int $year */ ?>
 
 @extends ('web')
 
 @push ('head')
     <x-og.website />
-    <x-link-pagination :paginator="$posts" />
 @endpush
 
 @section ('content')
-    <x-section>
-        <h1 class="mb-8 text-6xl leading-none font-black text-night-0 dark:text-white">Posts from {{ $year }}</h1>
-        <div class="mb-8 grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 lg:gap-10 xl:gap-12">
-            @foreach ($posts as $post)
-                <x-post.preview :post="$post"></x-post.preview>
-            @endforeach
-        </div>
-        <x-pagination :paginator="$posts"></x-pagination>
-    </x-section>
+    <statamic:collection:posts
+        :since="$year.'-01-01'"
+        :until="$year.'-12-31 23:59:59'"
+        sort="date:desc"
+        paginate="6"
+        as="posts"
+    >
+        @push ('head')
+            <x-link-pagination :paginate="$paginate" />
+        @endpush
+
+        <x-section>
+            <h1 class="mb-8 text-6xl leading-none font-black text-night-0 dark:text-white">Posts from {{ $year }}</h1>
+            <div class="mb-8 grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 lg:gap-10 xl:gap-12">
+                @foreach ($posts as $post)
+                    <x-post.preview :post="$post" />
+                @endforeach
+            </div>
+            <x-pagination :paginate="$paginate" />
+        </x-section>
+    </statamic:collection:posts>
 @endsection
