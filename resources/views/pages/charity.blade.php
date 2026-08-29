d5287c3f04ac10726a830f6e51eb21221d04ba3d
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 11:17:22 +0200

    Compose charity section from component

--- resources/views/pages/charity.blade.php
+++ resources/views/pages/charity.blade.php
@@ -1,5 +1,4 @@
 <?php /** @var Illuminate\Support\HtmlString $contents */ ?>
-<?php /** @var Illuminate\Support\Collection<int, Statamic\Entries\Entry> $charities */ ?>
 
 @extends ('web')
 
@@ -10,30 +9,5 @@
 @section ('content')
     <x-article class="prose md:prose-lg lg:prose-xl"> {!! $contents !!} </x-article>
 
-    <x-section class="bg-dotted">
-        <x-grid class="xl:grid-cols-4">
-            @foreach ($charities as $charity)
-                <div class="overflow-hidden rounded-4 bg-white shadow dark:bg-night-20">
-                    <a
-                        href="{{ $charity->value('url') }}"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                        class="block pb-1"
-                    >
-                        <x-figure>
-                            <x-img
-                                :src="$charity->image"
-                                width="768"
-                                ratio="16:9"
-                                :alt="$charity->value('title')"
-                            />
-                            <x-slot name="caption">
-                                {{ $charity->value('title') }}
-                            </x-slot>
-                        </x-figure>
-                    </a>
-                </div>
-            @endforeach
-        </x-grid>
-    </x-section>
+    <x-charity.organizations />
 @endsection
