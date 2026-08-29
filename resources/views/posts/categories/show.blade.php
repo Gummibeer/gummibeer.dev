9aa9d08ebfe23d99755d3dbe8ef3c4f6ee5b8ebd
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-30 00:16:03 +0200

    Use Statamic taxonomy route entries

--- resources/views/posts/categories/show.blade.php
+++ resources/views/posts/categories/show.blade.php
@@ -11,18 +11,14 @@
 @endpush
 
 @section ('content')
-    <statamic:collection:posts
-        taxonomy:categories="{{ $page->slug() }}"
-        sort="date:desc"
-        as="posts"
-    >
-        <x-section>
-            <h1 class="mb-8 text-6xl leading-none font-black text-night-0 dark:text-white">Posts about {{ $page->title }}</h1>
-            <div class="mb-8 grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 lg:gap-10 xl:gap-12">
-                @foreach ($posts as $post)
-                    <x-post.preview :post="$post" />
-                @endforeach
-            </div>
-        </x-section>
-    </statamic:collection:posts>
+    @php($posts = $entries->get())
+
+    <x-section>
+        <h1 class="mb-8 text-6xl leading-none font-black text-night-0 dark:text-white">Posts about {{ $page->title }}</h1>
+        <div class="mb-8 grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 lg:gap-10 xl:gap-12">
+            @foreach ($posts as $post)
+                <x-post.preview :post="$post" />
+            @endforeach
+        </div>
+    </x-section>
 @endsection
