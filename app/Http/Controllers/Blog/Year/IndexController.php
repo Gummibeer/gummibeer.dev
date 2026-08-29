22a747910a4149e3ab8ee833b77926b43becd3a9
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 13:07:00 +0200

    Let Statamic load year archive entries

--- app/Http/Controllers/Blog/Year/IndexController.php
+++ app/Http/Controllers/Blog/Year/IndexController.php
@@ -3,26 +3,14 @@
 namespace App\Http\Controllers\Blog\Year;
 
 use App\Services\MetaBag;
-use Carbon\Carbon;
-use Statamic\Facades\Entry;
+use Illuminate\Contracts\View\View;
 
 class IndexController
 {
-    public function __invoke(MetaBag $meta, int $year, int $page = 1)
+    public function __invoke(MetaBag $meta, int $year): View
     {
         $meta->title = sprintf('Posts from %d | Blog', $year);
 
-        $posts = Entry::query()
-            ->where('collection', 'posts')
-            ->whereStatus('published')
-            ->where('date', '>=', Carbon::create($year)->startOfYear())
-            ->where('date', '<=', Carbon::create($year)->endOfYear())
-            ->get()
-            ->sortByDesc(fn ($entry) => $entry->date())
-            ->values()
-            ->paginate($page)
-            ->withRoute('blog.year.index', compact('year'));
-
-        return view('pages.blog.year', compact('year', 'posts'));
+        return view('pages.blog.year', compact('year'));
     }
 }
