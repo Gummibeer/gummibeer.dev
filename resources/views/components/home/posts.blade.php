605ab8da6bac1872b6fda2af2e4d2c043fb4af0a
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 13:04:36 +0200

    Use Statamic collection tag for home posts

--- resources/views/components/home/posts.blade.php
+++ resources/views/components/home/posts.blade.php
@@ -1,19 +1,21 @@
-@if ($posts->isNotEmpty())
-    <x-section>
-        <x-post.promo :post="$posts->first()" />
-    </x-section>
-@endif
+<statamic:collection:posts sort="date:desc" limit="4" as="posts">
+    @if ($posts->isNotEmpty())
+        <x-section>
+            <x-post.promo :post="$posts->first()" />
+        </x-section>
+    @endif
 
-@if ($posts->skip(1)->isNotEmpty())
-    <x-section class="bg-dotted">
-        <h2 class="mb-8 text-4xl leading-none font-bold text-night-0 dark:text-white">Latest Posts</h2>
-        <x-grid>
-            @foreach ($posts->skip(1)->take(3) as $post)
-                <x-post.preview
-                    :post="$post"
-                    :class="$loop->iteration === 3 ? 'hidden lg:block' : ''"
-                />
-            @endforeach
-        </x-grid>
-    </x-section>
-@endif
+    @if ($posts->skip(1)->isNotEmpty())
+        <x-section class="bg-dotted">
+            <h2 class="mb-8 text-4xl leading-none font-bold text-night-0 dark:text-white">Latest Posts</h2>
+            <x-grid>
+                @foreach ($posts->skip(1) as $post)
+                    <x-post.preview
+                        :post="$post"
+                        :class="$loop->iteration === 3 ? 'hidden lg:block' : ''"
+                    />
+                @endforeach
+            </x-grid>
+        </x-section>
+    @endif
+</statamic:collection:posts>
