03cefc7a5d6b2948409cc5d1538cc12ade068fb2
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 11:16:05 +0200

    Move home post query into component

---
+++ resources/views/components/home/posts.blade.php
@@ -0,0 +1,28 @@
+@php
+    $posts = Statamic\Facades\Entry::query()
+        ->where('collection', 'posts')
+        ->whereStatus('published')
+        ->get()
+        ->sortByDesc(fn (Statamic\Entries\Entry $entry) => $entry->date())
+        ->values();
+@endphp
+
+@if ($posts->isNotEmpty())
+    <x-section>
+        <x-post.promo :post="$posts->first()" />
+    </x-section>
+@endif
+
+@if ($posts->skip(1)->isNotEmpty())
+    <x-section class="bg-dotted">
+        <h2 class="mb-8 text-4xl leading-none font-bold text-night-0 dark:text-white">Latest Posts</h2>
+        <x-grid>
+            @foreach ($posts->skip(1)->take(3) as $post)
+                <x-post.preview
+                    :post="$post"
+                    :class="$loop->iteration === 3 ? 'hidden lg:block' : ''"
+                />
+            @endforeach
+        </x-grid>
+    </x-section>
+@endif
