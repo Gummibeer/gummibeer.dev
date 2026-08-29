f4ebc6541e5b64498bdbaea99372bd192bebd094
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 13:13:26 +0200

    Remove blog archive links from post metadata

--- resources/views/components/post/aside.blade.php
+++ resources/views/components/post/aside.blade.php
@@ -6,20 +6,11 @@
     <ul class="flex list-none flex-col sm:flex-row sm:space-x-3">
         <li>
             <x-icon class="fal fa-calendar mr-1" />
-            <a
-                href="{{ route('blog.year.index', $post->date->year) }}"
-                class="hover:text-brand"
-            >
-                <time datetime="{{ $post->date->format('Y-m-d') }}"> {{ $post->date->format('M jS, Y') }} </time>
-            </a>
+            <time datetime="{{ $post->date->format('Y-m-d') }}"> {{ $post->date->format('M jS, Y') }} </time>
         </li>
         <li>
             <x-icon class="fal fa-clock mr-1" />
             {{ $post->read_time }} min read
         </li>
-        {{--        <li>--}}
-        {{--            <x-icon class="mr-1 fal fa-user"/>--}}
-        {{--            <a href="{{ route('blog.author.index', ['author' => $post->author->slug()]) }}" class="hover:text-brand">{{ $post->author->title }}</a>--}}
-        {{--        </li>--}}
     </ul>
 </aside>
