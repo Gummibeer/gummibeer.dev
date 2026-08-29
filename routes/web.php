e6066730e2e54f241431cf44b748f207c0e9ba98
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 08:55:48 +0200

    Let SEO Pro own sitemap metadata

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,8 @@
 
 use App\Http\Controllers\Blog;
 use App\Http\Middleware\Paginated;
-use App\Services\MetaBag;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
-use Spatie\Sitemap\SitemapGenerator;
-use Spatie\Sitemap\Tags\Url;
 
 Route::prefix('blog')->name('blog.')->group(function (): void {
     Route::get('search', Blog\SearchController::class)->name('search');
@@ -28,48 +25,15 @@ Route::prefix('blog')->name('blog.')->group(function (): void {
 
 Route::get(
     '404.html',
-    function (MetaBag $meta) {
-        $meta->title = 'Not Found';
-
-        return view('pages.404');
-    }
+    fn () => response()->view('pages.404', ['response_code' => 404], 404)
 )->name('404');
-
-Route::get(
-    'sitemap.xml',
-    fn () => SitemapGenerator::create(url('/'))
-        ->hasCrawled(function (Url $url): Url {
-            $url->setUrl(rtrim($url->url, '/'));
-
-            if (in_array($url->segment(1), ['resume', 'portfolio', 'charity', 'uses'])) {
-                $url
-                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
-                    ->setPriority(0.5);
-            }
-
-            if (in_array($url->segment(1), ['imprint', 'privacy'])) {
-                $url
-                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
-                    ->setPriority(0.1);
-            }
-
-            if ($url->segment(1) === 'blog') {
-                $url
-                    ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
-                    ->setPriority(1);
-            }
-
-            return $url;
-        })
-        ->getSitemap()
-)->name('sitemap.xml');
 
 Route::get('robots.txt', static function (): Response {
     $content = collect([
         'User-agent' => '*',
         'Allow' => '/',
         null,
-        'Sitemap' => route('sitemap.xml'),
+        'Sitemap' => url('/sitemap.xml'),
     ])
         ->map(fn (?string $value, string $key): string => $value ? "{$key}: {$value}" : '')
         ->implode(PHP_EOL);
