cebac79fb9d3554c69c9baf02e7a80d3b8d53c29
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-29 13:04:42 +0200

    Use Statamic collection tag for home streams

--- resources/views/components/home/streams.blade.php
+++ resources/views/components/home/streams.blade.php
@@ -1,8 +1,10 @@
-<x-section>
-    <h2 class="mb-8 text-4xl leading-none font-bold text-night-0 dark:text-white">Latest Streams</h2>
-    <x-grid>
-        @foreach ($streams->take(3) as $stream)
-            <x-stream.preview :stream="$stream" />
-        @endforeach
-    </x-grid>
-</x-section>
+<statamic:collection:streams sort="date:desc" limit="3" as="streams">
+    <x-section>
+        <h2 class="mb-8 text-4xl leading-none font-bold text-night-0 dark:text-white">Latest Streams</h2>
+        <x-grid>
+            @foreach ($streams as $stream)
+                <x-stream.preview :stream="$stream" />
+            @endforeach
+        </x-grid>
+    </x-section>
+</statamic:collection:streams>
