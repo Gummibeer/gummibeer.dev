ea774e609b3c55143a1708cb4d7dc70d40d78797
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-30 10:30:57 +0200

    fix category h1

--- resources/views/posts/categories/show.blade.php
+++ resources/views/posts/categories/show.blade.php
@@ -14,7 +14,7 @@
     @php ($posts = $entries->get())
 
     <x-section>
-        <h1 class="mb-8 text-6xl leading-none font-black text-night-0 dark:text-white">Posts about {{ $page->title }}</h1>
+        <h1 class="mb-8 text-6xl leading-none font-black text-night-0 dark:text-white">Posts about {{ str($page->title)->headline() }}</h1>
         <div class="mb-8 grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 lg:gap-10 xl:gap-12">
             @foreach ($posts as $post)
                 <x-post.preview :post="$post" />
