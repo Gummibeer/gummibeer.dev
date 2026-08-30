d704574a63a5a037e07c6457e523338b794f8bd9
Tom Herrmann <dev.gummibeer@gmail.com>
2026-08-30 22:42:43 +0200

    Clean obsolete route patterns

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -11,10 +11,6 @@ class RouteServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        Route::pattern('year', '[0-9]{4}');
-        Route::pattern('page', 'p:([0-9]+)');
-        Route::pattern('post', '[a-z0-9\\-]+');
-
         $this->routes(function (): void {
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
