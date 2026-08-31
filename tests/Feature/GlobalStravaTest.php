3a4c58dbb68709a3783824a97f559d76deb44a4c
Tom Witkowski <dev.gummibeer@gmail.com>
2026-08-31 21:19:50 +0200

    remove redundant Strava command test from GlobalStravaTest

--- tests/Feature/GlobalStravaTest.php
+++ tests/Feature/GlobalStravaTest.php
@@ -24,41 +24,4 @@ final class GlobalStravaTest extends TestCase
         $this->assertSame(0, $variables->get('elevation_gain'));
         $this->assertSame(0, $variables->get('moving_time'));
     }
-
-    public function test_strava_command_updates_the_global(): void
-    {
-        Http::fake([
-            'https://www.strava.com/api/v3/oauth/token*' => Http::response(['access_token' => 'token']),
-            'https://www.strava.com/api/v3/athletes/*/stats' => Http::response([
-                'all_ride_totals' => [
-                    'count' => 42,
-                    'distance' => 123456.7,
-                    'elevation_gain' => 890.4,
-                    'moving_time' => 7200,
-                ],
-            ]),
-        ]);
-
-        try {
-            $this->artisan('stats:strava')->assertSuccessful();
-
-            $strava = GlobalSet::findByHandle('strava');
-            $this->assertNotNull($strava);
-
-            $variables = $strava->inDefaultSite();
-            $this->assertSame(123457, $variables->get('distance'));
-            $this->assertSame(890, $variables->get('elevation_gain'));
-            $this->assertSame(7200, $variables->get('moving_time'));
-        } finally {
-            $strava = GlobalSet::findByHandle('strava');
-
-            if ($strava) {
-                $strava->inDefaultSite()->data([
-                    'distance' => 0,
-                    'elevation_gain' => 0,
-                    'moving_time' => 0,
-                ])->save();
-            }
-        }
-    }
 }
