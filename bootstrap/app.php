74ad7f90aafb7bcf41eb8122039569bfa935ddae
Tom Witkowski <dev.gummibeer@gmail.com>
2026-08-31 22:07:12 +0200

    remove unused Kernel, RouteServiceProvider, and related references after migrating to bootstrap configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AutoLoginStatamicControlPanel;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -8,11 +9,10 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->appendToGroup('web', AutoLoginStatamicControlPanel::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,10 +2,8 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\MacroServiceProvider;
-use App\Providers\RouteServiceProvider;
 
 return [
     AppServiceProvider::class,
-    RouteServiceProvider::class,
     MacroServiceProvider::class,
 ];
