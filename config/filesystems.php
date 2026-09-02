40f3f758d1eded59be5368fdcb950f39645f480d
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-02 09:20:48 +0200

    Update Laravel 13 config options

--- config/filesystems.php
+++ config/filesystems.php
@@ -33,6 +33,7 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app/private'),
+            'serve' => true,
             'visibility' => 'private',
             'throw' => true,
             'report' => true,
