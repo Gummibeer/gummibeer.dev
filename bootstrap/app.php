6784773ce241df017f784896216d980053442bce
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-02 23:02:43 +0200

    make public web middleware stateless

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(remove: [
+        $middleware->removeFromGroup('web', [
             EncryptCookies::class,
             AddQueuedCookiesToResponse::class,
             StartSession::class,
