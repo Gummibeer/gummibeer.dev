0a89b4deb89e1a2973d25255986331b933d17aeb
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-04 17:48:36 +0200

    Preserve prompt template whitespace

--- resources/views/components/prompt.blade.php
+++ resources/views/components/prompt.blade.php
@@ -3,6 +3,7 @@
 <?php /** @var string $prompt */ ?>
 <?php /** @var string $title */ ?>
 
+{{-- prettier-ignore --}}
 <div class="not-prose relative my-6 overflow-hidden rounded-2 border-2 border-snow-10 bg-white dark:border-night-20 dark:bg-night-10">
     <details>
         <summary class="cursor-pointer bg-snow-10 px-4 py-3 pr-24 font-bold dark:bg-night-20">{{ $title }}</summary>
