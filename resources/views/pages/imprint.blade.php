48321b673ae52498df2aebff90b5a0b54210c0d2
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-04 15:30:22 +0200

    Change content rendering to use {!! !!} syntax

--- resources/views/pages/imprint.blade.php
+++ resources/views/pages/imprint.blade.php
@@ -1,5 +1,5 @@
 @extends ('web')
 
 @section ('content')
-    <x-article class="prose md:prose-lg lg:prose-xl"> {{ $content }} </x-article>
+    <x-article class="prose md:prose-lg lg:prose-xl"> {!! $content !!} </x-article>
 @endsection
