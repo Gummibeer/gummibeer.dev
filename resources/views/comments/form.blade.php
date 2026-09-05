bc2425670a423fbadc92d9d50853d9ab11e79cef
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-05 22:55:41 +0200

    Use native Alpine comment form

--- resources/views/comments/form.blade.php
+++ resources/views/comments/form.blade.php
@@ -4,6 +4,7 @@
     method="POST"
     action="{{ \Statamic\Facades\Form::find('comments')->actionUrl() }}"
     class="space-y-4"
+    @submit.prevent="submit($event.currentTarget)"
 >
     <input type="hidden" name="post" value="{{ $post->id() }}" />
 
@@ -43,6 +44,7 @@
     <button
         type="submit"
         class="rounded-1 bg-brand px-4 py-2 font-bold text-night-0 shadow transition hover:brightness-95 focus:outline-none focus-visible:ring-2 focus-visible:ring-brand focus-visible:ring-offset-2 disabled:cursor-wait disabled:opacity-50"
+        :disabled="loading || widgetId === null"
     >
         Send comment
     </button>
