07857e5baa03a9b47bb6e131adcd0d699280700e
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-05 16:45:06 +0200

    prettier

--- resources/views/errors/404-markdown.blade.php
+++ resources/views/errors/404-markdown.blade.php
@@ -1,7 +1,1 @@
-# Not Found
-
-That page doesn't exist.
-
-- [Home]({{ url('/') }})
-- [Sitemap]({{ route('sitemap.xml') }})
-- [llms.txt]({{ url('/llms.txt') }})
+# Not Found That page doesn't exist. - [Home]({{ url('/') }}) - [Sitemap]({{ route('sitemap.xml') }}) - [llms.txt]({{ url('/llms.txt') }})
