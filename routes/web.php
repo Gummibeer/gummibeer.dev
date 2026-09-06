b317189959fa0d3ddd749fa11456ebfc97aff2b7
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-06 13:38:12 +0200

    Replace collection feeds with journal feed

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,14 @@
 <?php
 
-use App\Http\Controllers\Blog\Category\FeedController as CategoryFeedController;
-use App\Http\Controllers\Blog\FeedController as BlogFeedController;
 use App\Http\Controllers\GetMarkdownController;
 use App\Http\Controllers\GetSitemapController;
-use App\Http\Controllers\Streams\FeedController as StreamsFeedController;
+use App\Http\Controllers\Journal\FeedController as JournalFeedController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('blog.')->group(function (): void {
-    Route::statamic('blog/search', 'pages.blog.search')->name('search');
-    Route::get('blog.{format}', BlogFeedController::class)->whereIn('format', ['atom', 'rss'])->name('feed');
-    Route::get('blog/categories/{category}.{format}', CategoryFeedController::class)->whereIn('format', ['atom', 'rss'])->name('category.feed');
-});
-
-Route::name('streams.')->group(function (): void {
-    Route::get('streams.{format}', StreamsFeedController::class)->whereIn('format', ['atom', 'rss'])->name('feed');
-});
+Route::statamic('blog/search', 'pages.blog.search')->name('blog.search');
+Route::get('journal.{format}', JournalFeedController::class)
+    ->whereIn('format', ['atom', 'rss'])
+    ->name('journal.feed');
 
 Route::get('{uri}.md', GetMarkdownController::class)
     ->where('uri', '.*')
