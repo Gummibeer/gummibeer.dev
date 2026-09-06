d0c9e913c803e88ee8562df0257f2de8e62f8d7d
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-06 12:28:50 +0200

    Make blog index posts only

--- resources/views/components/blog/entries.blade.php
+++ resources/views/components/blog/entries.blade.php
@@ -1,8 +1,7 @@
-<statamic:collection
-    from="posts|streams"
+<statamic:collection:posts
     sort="date:desc"
     paginate="12"
-    as="entries"
+    as="posts"
 >
     @push ('head')
         <x-link-pagination :paginate="$paginate" />
@@ -13,14 +12,10 @@
         <h1 class="mb-8 text-6xl leading-none font-black text-night-0">Blog</h1>
         <x-post.search />
         <div class="mb-8 grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3 lg:gap-10 xl:gap-12">
-            @foreach ($entries as $entry)
-                @if ($entry->collection()->handle() === 'posts')
-                    <x-post.preview :post="$entry" />
-                @elseif ($entry->collection()->handle() === 'streams')
-                    <x-stream.preview :stream="$entry" />
-                @endif
+            @foreach ($posts as $post)
+                <x-post.preview :post="$post" />
             @endforeach
         </div>
         <x-pagination :paginate="$paginate" />
     </x-section>
-</statamic:collection>
+</statamic:collection:posts>
