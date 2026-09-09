93a00cbd05c12c46ba7bd8005009d7a2f52d1886
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-09 23:34:32 +0200

    Register security headers middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\AddContentSecurityPolicyHeaders;
 use App\Http\Middleware\AddLinkHeaders;
+use App\Http\Middleware\AddSecurityHeaders;
 use App\Http\Middleware\NegotiateMarkdown;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -29,6 +30,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->prependToGroup('web', AddLinkHeaders::class);
         $middleware->appendToGroup('web', AddContentSecurityPolicyHeaders::class);
+        $middleware->appendToGroup('web', AddSecurityHeaders::class);
         $middleware->appendToGroup('web', NegotiateMarkdown::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
