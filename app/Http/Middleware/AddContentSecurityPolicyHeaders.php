b1323946dc32e271eaad812b08ff811faa27c7e7
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-09 15:14:43 +0200

    Skip CSP only for enabled control panel

--- app/Http/Middleware/AddContentSecurityPolicyHeaders.php
+++ app/Http/Middleware/AddContentSecurityPolicyHeaders.php
@@ -5,15 +5,17 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Spatie\Csp\AddCspHeaders;
+use Symfony\Component\HttpFoundation\Response;
 
 class AddContentSecurityPolicyHeaders extends AddCspHeaders
 {
-    public function handle(Request $request, Closure $next, ?string $customPreset = null)
+    public function handle(Request $request, Closure $next, ?string $customPreset = null): Response
     {
         $controlPanelRoute = trim((string) config('statamic.cp.route'), '/');
 
         if (
-            $controlPanelRoute !== ''
+            config('statamic.cp.enabled')
+            && $controlPanelRoute !== ''
             && $request->is($controlPanelRoute, $controlPanelRoute.'/*')
         ) {
             return $next($request);
