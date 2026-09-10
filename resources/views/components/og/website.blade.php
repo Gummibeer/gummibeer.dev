9961d63c9505bb69c39ef213bcecbd3d4bf0d5a8
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-10 21:59:35 +0200

    Use computed OG image in website metadata

--- resources/views/components/og/website.blade.php
+++ resources/views/components/og/website.blade.php
@@ -4,7 +4,8 @@
 ])
 
 @php
-    $image = \App\Services\OpenGraphImage::url($page);
+    $image = $page?->og_image ?? null;
+    $image = $image ? \App\Services\OpenGraphImage::url((string) $image) : null;
     $imageAlt = $page?->title ?? $identity->name;
 @endphp
 
@@ -24,26 +25,28 @@
     property="og:locale"
     content="{{ str_replace('-', '_', app()->getLocale()) }}"
 />
-<meta
-    property="og:image"
-    content="{{ $image }}"
-/>
-<meta
-    property="og:image:type"
-    content="image/png"
-/>
-<meta
-    property="og:image:width"
-    content="{{ \App\Services\OpenGraphImage::WIDTH }}"
-/>
-<meta
-    property="og:image:height"
-    content="{{ \App\Services\OpenGraphImage::HEIGHT }}"
-/>
-<meta
-    property="og:image:alt"
-    content="{{ $imageAlt }}"
-/>
+@if ($image)
+    <meta
+        property="og:image"
+        content="{{ $image }}"
+    />
+    <meta
+        property="og:image:type"
+        content="image/png"
+    />
+    <meta
+        property="og:image:width"
+        content="{{ \App\Services\OpenGraphImage::WIDTH }}"
+    />
+    <meta
+        property="og:image:height"
+        content="{{ \App\Services\OpenGraphImage::HEIGHT }}"
+    />
+    <meta
+        property="og:image:alt"
+        content="{{ $imageAlt }}"
+    />
+@endif
 @if ($page?->description)
     <meta
         property="og:description"
@@ -52,20 +55,22 @@
 @endif
 <meta
     name="twitter:card"
-    content="summary_large_image"
+    content="{{ $image ? 'summary_large_image' : 'summary' }}"
 />
 <meta
     name="twitter:title"
     content="{{ $page?->title ? $page->title.' | '.$identity->name : $identity->name }}"
 />
-<meta
-    name="twitter:image"
-    content="{{ $image }}"
-/>
-<meta
-    name="twitter:image:alt"
-    content="{{ $imageAlt }}"
-/>
+@if ($image)
+    <meta
+        name="twitter:image"
+        content="{{ $image }}"
+    />
+    <meta
+        name="twitter:image:alt"
+        content="{{ $imageAlt }}"
+    />
+@endif
 @if ($page?->description)
     <meta
         name="twitter:description"
