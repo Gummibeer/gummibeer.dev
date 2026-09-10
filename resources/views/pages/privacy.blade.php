0c7bf621dd0c6150b6d83969c6b457b29d17fe7f
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-10 19:19:55 +0200

    Add OG metadata to privacy page

--- resources/views/pages/privacy.blade.php
+++ resources/views/pages/privacy.blade.php
@@ -1,5 +1,9 @@
 @extends ('web')
 
+@push ('head')
+    <x-og.website />
+@endpush
+
 @section ('content')
     <x-article class="prose max-w-none md:prose-lg lg:prose-xl"> {!! $content !!} </x-article>
 @endsection
