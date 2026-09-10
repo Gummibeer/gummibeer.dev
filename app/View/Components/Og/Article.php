14d1e7dde991e811977ad22ff67afe9778b78c59
Tom Herrmann <dev.gummibeer@gmail.com>
2026-09-10 19:19:14 +0200

    Attach generated OG images to articles

--- app/View/Components/Og/Article.php
+++ app/View/Components/Og/Article.php
@@ -2,6 +2,7 @@
 
 namespace App\View\Components\Og;
 
+use App\Services\OpenGraphImage;
 use Astrotomic\OpenGraph\OpenGraph;
 use Astrotomic\OpenGraph\Twitter;
 use Illuminate\View\Component;
@@ -23,15 +24,18 @@ class Article extends Component
     {
         $title = $this->post->value('title').' | '.$this->name;
         $description = (string) $this->post->value('description');
+        $image = OpenGraphImage::url($this->post);
 
         return implode(PHP_EOL, [
             OpenGraph::article($title)
                 ->url((string) $this->post->absoluteUrl())
+                ->image($image)
                 ->when($description)->description($description)
                 ->publishedAt($this->post->date())
                 ->modifiedAt($this->post->last_modified_at->toDateTime())
                 ->locale(str_replace('-', '_', app()->getLocale())),
             Twitter::summaryLargeImage($title)
+                ->image($image, (string) $this->post->value('title'))
                 ->when($description)->description($description),
         ]);
     }
